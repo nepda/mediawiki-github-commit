f4f44c3f882920b9c449b92f6bb80979d41839ed
Nepomuk Fraedrich (nn) <info@nepda.eu>
2012-07-08 19:59:22 +0200

    finish, wikipage finish

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ mediawiki-github-commit
 =======================
 
 lists defined count of commit messages of an given repo hosted on github.com
+
+Linux installation
+==================
+ 
+ // cd /path/to/your/wiki/
+ cd extensions
+ git clone https://github.com/nepda/mediawiki-github-commit.git
+ 
+open LocalSettings.php and add this line somewhere (at the end)
+
+ require_once("$IP/extensions/mediawiki-github-commit/github-commit.php");
+ 
+Now you can fetch github-commit messages:
+
+ <githubcommit user="nepda" repo="mediawiki-github-commit" offset="0" count="10">
--- github-commit.php
+++ github-commit.php
@@ -4,31 +4,36 @@
  *
  */
 
-if( !defined( 'MEDIAWIKI' ) ) {
-	echo( "This file is part of an extension to the MediaWiki software and cannot be used standalone.\n" );
-	die( 1 );
+if(!defined('MEDIAWIKI'))
+{
+    echo( "This file is part of an extension to the MediaWiki software and cannot be used standalone.\n" );
+    die( 1 );
 }
 
 $wgExtensionCredits['other'][] = array(
-        'name'=>'github.com commitLister',
-        'author'=>'Nepomuk Fraedrich',
-        'description'=>'lists defined count of commit messages of an given repo hosted on github.com',
-        'version'=>'0.1'
+    'name'=>'github.com commitLister',
+    'author'=>'Nepomuk Fraedrich',
+    'description'=>'lists defined count of commit messages of an given repo hosted on github.com',
+    'version'=>'0.1'
 );
 
-if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
-        $wgHooks['ParserFirstCallInit'][] = 'efGitHubCommit';
+if (defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT')) 
+{
+    $wgHooks['ParserFirstCallInit'][] = 'efGitHubCommit';
 } else {
-        $wgExtensionFunctions[] = 'efGitHubCommit';
+    
+    $wgExtensionFunctions[] = 'efGitHubCommit';
 }
 
-function efGitHubCommit() {
-        global $wgParser;
-        $wgParser->setHook( 'githubcommit', 'efGitHubCommitRender' );
-        return true;
+function efGitHubCommit()
+{
+    global $wgParser;
+    $wgParser->setHook('githubcommit', 'efGitHubCommitRender');
+    return true;
 }
 
-function efGitHubCommitRender( $input, $args, $parser )
+
+function efGitHubCommitRender($input, $args, $parser)
 {
     $attr = array();
     $offset = 0;
@@ -42,25 +47,25 @@ function efGitHubCommitRender( $input, $args, $parser )
 
     foreach( $args as $name => $value )
     {
-	switch($name)
-	{
-    case 'offset':
-					$offset = (int)$value;
-					break;
-				case 'count':
-					$count = (int)$value;
-					break;
-				case 'user':
-					$user = $value;
-					break;
-				case 'repo':
-					$repo = $value;
-					break;
-				case 'dateformat':
-					$date_format = $value;
-					break;
-			}
+        switch($name)
+        {
+            case 'offset':
+                $offset = (int)$value;
+                break;
+            case 'count':
+                $count = (int)$value;
+                break;
+            case 'user':
+                $user = $value;
+                break;
+            case 'repo':
+                $repo = $value;
+                break;
+            case 'dateformat':
+                $date_format = $value;
+                break;
         }
+    }
 
 
     $tpl_before = "<ul>";
@@ -68,38 +73,39 @@ function efGitHubCommitRender( $input, $args, $parser )
     $tpl_after = "</ul>";
     
 
-		$cfg_api_url = 'https://api.github.com/repos/'.$user.'/'.$repo.'/commits';
+    $cfg_api_url = 'https://api.github.com/repos/'.$user.'/'.$repo.'/commits';
 
-		$json = file_get_contents($cfg_api_url);
+    $json = file_get_contents($cfg_api_url);
 
-		$data = json_decode($json);
-
-
-		$str .= $tpl_before;
-		for($i = $offset; $i <= $count; $i++)
-		{
-		    if (empty($data[$i]))
-			continue;
-		    $commit = array(
-			'%commit_url%' => $data[$i]->commit->url,
-			'%commit_committer_email%' => $data[$i]->commit->committer->email,
-			'%commit_committer_date%' => date($date_format, strtotime($data[$i]->commit->committer->date)),
-			'%commit_committer_name%' => $data[$i]->commit->committer->name,
-			'%commit_message%' => $data[$i]->commit->message,
-			'%commit_tree_url%' => $data[$i]->commit->tree->url,
-			'%commit_tree_sha%' => $data[$i]->commit->tree->sha,
-			'%commit_sha%' => $data[$i]->commit->sha,
-			'%commit_author_email%' => $data[$i]->commit->author->email,
-			'%commit_author_date%' => date($date_format, strtotime($data[$i]->commit->author->date)),
-			'%commit_author_name%' => $data[$i]->commit->author->name,
-		    );
-
-		    $tmp_tpl = str_replace(array_keys($commit), array_values($commit), $tpl);
-		    
-		    $str .= $tmp_tpl;
-		}
-		$str .= $tpl_after;
+    $data = json_decode($json);
 
 
-        return $str;
+    $str .= $tpl_before;
+    for($i = $offset; $i <= $count; $i++)
+    {
+        if (empty($data[$i]))
+            continue;
+
+        $commit = array(
+            '%commit_url%' => $data[$i]->commit->url,
+            '%commit_committer_email%' => $data[$i]->commit->committer->email,
+            '%commit_committer_date%' => date($date_format, strtotime($data[$i]->commit->committer->date)),
+            '%commit_committer_name%' => $data[$i]->commit->committer->name,
+            '%commit_message%' => $data[$i]->commit->message,
+            '%commit_tree_url%' => $data[$i]->commit->tree->url,
+            '%commit_tree_sha%' => $data[$i]->commit->tree->sha,
+            '%commit_sha%' => $data[$i]->commit->sha,
+            '%commit_author_email%' => $data[$i]->commit->author->email,
+            '%commit_author_date%' => date($date_format, strtotime($data[$i]->commit->author->date)),
+            '%commit_author_name%' => $data[$i]->commit->author->name
+        );
+
+        $tmp_tpl = str_replace(array_keys($commit), array_values($commit), $tpl);
+
+        $str .= $tmp_tpl;
+    }
+    $str .= $tpl_after;
+
+
+    return $str;
 }
